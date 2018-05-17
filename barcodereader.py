b9b5baf08434f02e4b67b6c10ab013c0aa11e81d
Darren Draper <darren.draper@gpsengine.net>
2018-05-17 12:11:15 +1000

    Initial commit.

---
+++ barcodereader.py
@@ -0,0 +1,12 @@
+# Python Barcode Reader
+# Scan the Barcode or QR Code
+# Read the data
+# Display the data
+
+import zbar
+import numpy
+
+print ("Bar Code Reader")
+
+print ("Point the phone's camera towards the barcode/QR code")
+
